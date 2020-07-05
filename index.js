d498e3287041d66f464d1020ce24de7a1892bc8a
PreFiXAUT <mail@prefix.moe>
2020-07-05 22:30:14 +0200

    Cleanup

--- index.js
+++ index.js
@@ -19,43 +19,25 @@ module.exports = (api, options) => {
                 '--meta': 'Specifies the meta-file to bunlde up, instead of creating one from the package.json',
                 '--skip-meta': 'If it should skip the inclusion of the meta-file completely',
                 '--output':
-                    'Specifies the output-file of the bundled plugin-package. Defaults to "[package-name].splplg"',
+                    'Specifies the output-file of the bundled plugin-package. Defaults to "[package-name].[ext]"',
             },
         },
         args => {
-            const pkg = JSON.parse(fs.readFileSync(api.resolve('package.json')));
-            let outFile = api.resolve(`${pkg.name}.splplg`);
-            let filesGlob = `**/*`;
-            let meta = {
-                name: pkg.name,
-                version: pkg.version,
-                author: pkg.author != null && typeof pkg.author === 'object' ? pkg.author.name : pkg.author,
-                compatibleVersion: pkg.splitterino.compatibleVersion,
-                repositoryURL: pkg.splitterino.repositoryURL,
+            let ext;
+
+            if (args.plugin !== true && args.template !== true) {
+                throw new Error('You need to specify which bundle method to use ("--plugin" or "--template")!');
+            } else if (args.plugin === args.template) {
+                throw new Error('You may only specify one bundle method!');
+            } else if (args.plugin) {
+                ext = '.splplg';
+            } else if (args.template) {
+                ext = '.spltpl';
             }
 
-            if (typeof args.files === 'string') {
-                filesGlob = args.files;
-            }
-
-            if (!args['skip-meta'] && typeof args.meta === 'string') {
-                try {
-                    meta = JSON.parse(fs.readFileSync(path.resolve(args.meta)));
-                } catch (err) {
-                    console.error(`The file "${args.meta}" could not be found/loaded!`);
-
-                    return false;
-                }
-            }
-
-            if (typeof ((options.pluginOptions || {}).splitterino || {}).bundleFile === 'string') {
-                outFile = api.resolve(options.pluginOptions.splitterino.bundleFile);
-            }
-
-            if (typeof args.output == 'string') {
-                outFile = path.resolve(args.output);
-            }
-
+            const outFile = getOutFile(api, options, args, ext);
+            const meta = getMeta(api, options);
+            const filesGlob = getFilesGlob(args);
             const outStream = fs.createWriteStream(outFile);
 
             tarFs
@@ -63,6 +45,7 @@ module.exports = (api, options) => {
                     entries: glob.sync(filesGlob, {
                         root: api.resolve(`./${options.outputDir}`),
                     }),
+                    ignore: name => path.extname(name) === '.splplg',
                     finalize: false,
                     finish: pack => {
                         if (!args['skip-meta']) {
@@ -75,3 +58,49 @@ module.exports = (api, options) => {
         }
     )
 };
+
+function getMeta(args) {
+    let meta = {
+        name: pkg.name,
+        version: pkg.version,
+        author: pkg.author != null && typeof pkg.author === 'object' ? pkg.author.name : pkg.author,
+        compatibleVersion: pkg.splitterino.compatibleVersion,
+        repositoryURL: pkg.splitterino.repositoryURL,
+    }
+
+    if (!args['skip-meta'] && typeof args.meta === 'string') {
+        try {
+            meta = JSON.parse(fs.readFileSync(path.resolve(args.meta)));
+        } catch (err) {
+            throw new Error(`The file "${args.meta}" could not be found/loaded!`);
+        }
+    }
+
+    return meta;
+}
+
+function getOutFile(api, options, args, ext) {
+    const pkg = JSON.parse(fs.readFileSync(api.resolve('package.json')));
+    const normalizedName = pkg.name.replace(/[^a-zA-Z0-9_\-@\$]/g, '-');
+    let outFile = api.resolve(`./${options.outputDir}/${normalizedName}${ext}`);
+
+    if (typeof ((options.pluginOptions || {}).splitterino || {}).bundleFile === 'string') {
+        outFile = api.resolve(options.pluginOptions.splitterino.bundleFile);
+    }
+
+    if (typeof args.output == 'string') {
+        outFile = path.resolve(args.output);
+    }
+
+    return outFile;
+}
+
+function getFilesGlob(args) {
+    let filesGlob = `**/*`;
+
+    if (typeof args.files === 'string') {
+        filesGlob = args.files;
+    }
+
+    return filesGlob;
+}
